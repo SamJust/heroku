9a34b8d45aef6ce25408703aa8ff055543f78fad
unknown <victor.bezrodniy@peakwork.onmicrosoft.com>
2019-08-29 20:40:01 +0300

    messsage

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const jwt = require('jsonwebtoken');
 const app = require('express')();
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+const path = require('path');
 
 app.get('/', (req, res) => {
+    const filename = req.url === '/'? '/index.html' : req.url;
+    console.log(filename);
+    
+    const stream = fs.createReadStream(path.join(__dirname, '/static', filename));
+    stream.pipe(res);
+})
+
+app.get('/hello', (req, res) => {
     res.send('Hello, world!');
 });
 
---
+++ static/precache-manifest.df6bea02ccf74b9067dc506721e6f01b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3226614e2a9d58a291796df4bdbb5b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b423c510ba0ad2037c8",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ececc5eeb6f0d8924223",
+    "url": "/static/js/2.959e2451.chunk.js"
+  },
+  {
+    "revision": "9b423c510ba0ad2037c8",
+    "url": "/static/js/main.5d5346c4.chunk.js"
+  },
+  {
+    "revision": "a077a75fef86e50ee8d2",
+    "url": "/static/js/runtime~main.50e6d88c.js"
+  }
+]);
